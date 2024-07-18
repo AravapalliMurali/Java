05e9d9715e98d68366d5ad691b13bba6b07888af
muraliaravapalli <maravapalli@digisprint.com>
2024-07-18 08:41:36 +0530

    adding the hello world program

---
+++ Basic/Hello.java
@@ -0,0 +1,9 @@
+/**
+ * Hello
+ */
+public class Hello { // mian class 
+    public static void main(String ar[]) { // main method 
+        System.out.print("hello world"); // printing the hello world 
+    }
+    
+}
