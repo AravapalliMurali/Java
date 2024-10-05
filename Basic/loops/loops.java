48730a89f835deac39b406b47f4a9a32a665be5e
AravapalliMurali <aravapallimurali.am@gmail.com>
2024-10-05 23:25:30 +0530

    adding loops floder

---
+++ Basic/loops/loops.java
@@ -0,0 +1,9 @@
+public class loops {
+    public static void main(String[] args) {
+        int count = 1;
+        while (count >= 5) {
+            System.out.println("Hi" + count);
+            count++;
+        }
+    }
+}
