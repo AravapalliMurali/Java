c1c9ea28b5ea24440d509ef31b83a60d0dab0f78
AravapalliMurali <aravapallimurali.am@gmail.com>
2024-10-09 18:46:09 +0530

    worked on few new concepts

---
+++ javatraining/src/opps/Constructor/Demos.java
@@ -0,0 +1,10 @@
+
+public class Demos {
+    public static void main(String[] args) {
+        
+        Human h1 = new Human();
+
+        //System.out.println(h2.age + " : " + h2.name);
+    } 
+}
+
---
+++ javatraining/src/opps/Constructor/Human.java
@@ -0,0 +1,17 @@
+
+class Human {
+    public int age;
+    public String name;
+
+    // default constructor
+    public Human() {
+        this.age = 26;
+        this.name = "murali";
+        System.out.println("Inside constructor ");
+    }
+
+    public Human(int aga, String name) { // Parameterized constructor
+        this.age = age;
+        this.name = name;
+    }
+}
